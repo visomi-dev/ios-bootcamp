7d48f9c572c5cb47e7719a2dd480371224b750b6
jonathanavilap1 <40183554+jonathanavilap1@users.noreply.github.com>
2022-01-27 21:52:43 -0600

    First Commit, Struct of DrinkModel

---
+++ DrinkApp/DrinksModel.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+struct drinks{
+    
+    let idDrink: String
+    let name: String
+    let type: String
+    let glassType: String
+    let instructions: String
+    let image: URL
+    let ingredients: String
+    let measure: String
+    
+}
